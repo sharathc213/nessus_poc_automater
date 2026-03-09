11bcea509ec1a52124bbdf7dfcd817fc9dc32393
Sharath c <csharath213@gmail.com>
2026-03-09 13:35:06 +0530

    new plugin 70658

---
+++ plugins/70658.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+IP=$1
+PORT=$2
+SERVICE=$3
+NAME=$4
+OUTDIR=$5
+
+
+DIR="$OUTDIR/$NAME"
+TARGET_DIR="$DIR/${IP}-${PORT}"
+
+mkdir -p "$TARGET_DIR"
+
+OUTFILE="$TARGET_DIR/${IP}-${PORT}.txt"
+
+CMD="nmap -p $PORT -Pn --script ssh2-enum-algos $IP"
+
+echo "Target: $IP:$PORT" > "$OUTFILE"
+echo "Plugin: SSH Server CBC Mode Ciphers Enabled (70658)" >> "$OUTFILE"
+echo "" >> "$OUTFILE"
+
+echo "Command:" >> "$OUTFILE"
+echo "$CMD" >> "$OUTFILE"
+echo "" >> "$OUTFILE"
+
+echo "Output:" >> "$OUTFILE"
+
+timeout 120 bash -c "$CMD" >> "$OUTFILE" 2>&1
